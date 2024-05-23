4f826119b8512fbdc06bf8ad525db89dd4ca36ed
Shreejit Murthy <shreejitmurthy@gmail.com>
2024-05-23 21:19:21 +0930

    moved math and shader code

--- Engine/src/graphics.cpp
+++ Engine/src/graphics.cpp
@@ -1,90 +1,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "../vendor/stb_image.h"
 #include "graphics.hpp"
-
-void scale_image();
-
-void checkCompileErrors(unsigned int shader, std::string type) {
-    int success;
-    char infoLog[1024];
-    if (type != "PROGRAM")
-    {
-        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-        if (!success)
-        {
-            glGetShaderInfoLog(shader, 1024, NULL, infoLog);
-            std::cerr << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
-        }
-    }
-    else
-    {
-        glGetProgramiv(shader, GL_LINK_STATUS, &success);
-        if (!success)
-        {
-            glGetProgramInfoLog(shader, 1024, NULL, infoLog);
-            std::cerr << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
-        }
-    }
-}
-
-S_Shader new_shader(const char* vertexPath, const char* fragmentPath) {
-    S_Shader shader;
-     // 1. retrieve the vertex/fragment source code from filePath
-    std::string vertexCode;
-    std::string fragmentCode;
-    std::ifstream vShaderFile;
-    std::ifstream fShaderFile;
-    // ensure ifstream objects can throw exceptions:
-    vShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
-    fShaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
-    try {
-        // open files
-        vShaderFile.open(vertexPath);
-        fShaderFile.open(fragmentPath);
-        std::stringstream vShaderStream, fShaderStream;
-        // read file's buffer contents into streams
-        vShaderStream << vShaderFile.rdbuf();
-        fShaderStream << fShaderFile.rdbuf();
-        // close file handlers
-        vShaderFile.close();
-        fShaderFile.close();
-        // convert stream into string
-        vertexCode   = vShaderStream.str();
-        fragmentCode = fShaderStream.str();
-    }
-    catch (std::ifstream::failure& e) {
-        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ: " << e.what() << std::endl;
-    }
-    const char* vShaderCode = vertexCode.c_str();
-    const char * fShaderCode = fragmentCode.c_str();
-    // 2. compile shaders
-    unsigned int vertex, fragment;
-    // vertex shader
-    vertex = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex, 1, &vShaderCode, NULL);
-    glCompileShader(vertex);
-    checkCompileErrors(vertex, "VERTEX");
-    // fragment Shader
-    fragment = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment, 1, &fShaderCode, NULL);
-    glCompileShader(fragment);
-    checkCompileErrors(fragment, "FRAGMENT");
-    // shader Program
-    shader.ID = glCreateProgram();
-    glAttachShader(shader.ID, vertex);
-    glAttachShader(shader.ID, fragment);
-    glLinkProgram(shader.ID);
-    checkCompileErrors(shader.ID, "PROGRAM");
-    // delete the shaders as they're linked into our program now and no longer necessary
-    glDeleteShader(vertex);
-    glDeleteShader(fragment);
-
-    return shader;
-}
-
-void use_shader(S_Shader shader) {
-    glUseProgram(shader.ID); 
-}
+#include "math.hpp"
 
 void setVPT(int w, int h) {
     glViewport(0, 0, w, h);
@@ -93,11 +10,16 @@ void setVPT(int w, int h) {
 void Graphics::Texture::scale(float sx, float sy) {
     this->sx = sx;
     this->sy = sy;
-    this->scaled = true;
 }
 
-void Graphics::Texture::set_position(float x, float y, int screenWidth, int screenHeight) {
-    transform = glm::translate(transform, Math::ConvertTo3DSpace(glm::vec2(x, y), 1.f, screenWidth, screenHeight));
+void Graphics::Texture::set_position(float x, float y) {
+    this->px = x;
+    this->py = y;
+    this->pos_set = true;
+}
+
+void Graphics::Texture::rotate(float r) {
+    this->r = r;
 }
 
 // load glad + setup vertex data (and buffer(s)) and configure vertex attributess
@@ -117,14 +39,17 @@ Graphics::~Graphics() {
 }
 
 void Graphics::setupBuffers() {
-    static constexpr float vertices[] = {
+        // float
+    static constexpr auto vertices = std::array{
         // positions          // colours          // texture coords
         1.0f,  1.0f, 0.0f,    1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
         1.0f, -1.0f, 0.0f,    0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
         -1.0f, -1.0f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
         -1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
     };
-    static constexpr unsigned int indices[] = {
+
+    // unsigned int
+    static constexpr auto indices = std::array{
         0, 1, 3, // first triangle
         1, 2, 3  // second triangle
     };
@@ -134,18 +59,22 @@ void Graphics::setupBuffers() {
     glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
 
-    glEnableVertexAttribArray(0); // Position attribute
+    // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(1); // Color attribute
+    glEnableVertexAttribArray(0);
+    // colour attribute
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(2); // Texture coord attribute
+    glEnableVertexAttribArray(1);
+    // texture coord attribute
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 }
 
 Graphics::Texture Graphics::loadImage(const char* path, TextureFilter filter) {
@@ -173,19 +102,22 @@ void Graphics::clearBackground(Colour color) {
 }
 
 void Graphics::drawImage(Texture& texture, float x, float y) {
-    // Reset the transform to the identity matrix for each frame's drawing
     texture.transform = glm::mat4(1.0f);
 
-    // Set position via the texture's own method
-    texture.set_position(x, y, screenWidth, screenHeight);
+    texture.set_position(x, y);
+    texture.transform = glm::translate(texture.transform, Math::ConvertTo3DSpace(glm::vec2(texture.px, texture.py), 1.f, screenWidth, screenHeight));
 
-    // If the texture hasn't been manually scaled, calculate the scaling based on screen size
-    if (!texture.scaled) {
+    // check if scale has been set and scale appropriately.
+    if (texture.sx == 1.f && texture.sy == 1.f) {
         float scaleX = static_cast<float>(texture.width) / screenWidth;
         float scaleY = static_cast<float>(texture.height) / screenHeight;
         texture.transform = glm::scale(texture.transform, glm::vec3(scaleX, scaleY, 1.f));
     } else {
          texture.transform = glm::scale(texture.transform, glm::vec3(texture.sx, texture.sy, 1.f));
+    }
+
+    if (texture.r >= 0) {
+        texture.transform = glm::rotate(texture.transform, texture.r, glm::vec3(texture.ox, texture.oy, 1.0f));
     }
 
     // Apply the transformations and draw
--- Engine/src/graphics.hpp
+++ Engine/src/graphics.hpp
@@ -5,6 +5,8 @@
 #include "../vendor/glm/gtc/matrix_transform.hpp"
 #include "../vendor/glm/gtc/type_ptr.hpp"
 
+#include "shader.hpp"
+
 #include <iostream>
 #include <memory>
 #include <array>
@@ -16,58 +18,6 @@
 #define BLACK (Colour) {0.0f, 0.0f, 0.0f, 1.0f}
 #define TEAL  (Colour) {0.2f, 0.3f, 0.3f, 1.0f}
 
-class Math {
-public:
-    // Using GLM's vec2 and vec3 types
-    using Vec2 = glm::vec2;
-    using Vec3 = glm::vec3;
-
-    // Convert 3D vertex to 2D screen space
-    static Vec2 ConvertToScreenSpace(const Vec3& vertex, int screenWidth, int screenHeight) {
-        // Projection and normalization are simplified using GLM operations
-        Vec2 screenSpaceVertex = Vec2((vertex.x + 1.0f) * 0.5f * screenWidth,
-                                      (1.0f - vertex.y) * 0.5f * screenHeight);
-        return screenSpaceVertex;
-    }
-
-    static Vec3 ConvertTo3DSpace(const Vec2& screenVertex, float z, int screenWidth, int screenHeight) {
-        // Reversing screen space normalization
-        Vec3 normalizedVertex = Vec3((2.0f * screenVertex.x / screenWidth) - 1.0f,
-                                     1.0f - (2.0f * screenVertex.y / screenHeight),
-                                     z);
-        return normalizedVertex;
-    }
-
-    static std::vector<Vec2> ConvertVerticesToScreenSpace(const std::vector<Vec3>& vertices, int screenWidth, int screenHeight) {
-        std::vector<Vec2> screenSpaceVertices;
-        screenSpaceVertices.reserve(vertices.size());
-
-        for (const auto& vertex : vertices) {
-            screenSpaceVertices.push_back(ConvertToScreenSpace(vertex, screenWidth, screenHeight));
-        }
-
-        return screenSpaceVertices;
-    }
-
-    static std::vector<Vec3> ConvertScreenSpaceToVertices(const std::vector<Vec2>& screenSpaceVertices, float z, int screenWidth, int screenHeight) {
-        std::vector<Vec3> vertices;
-        vertices.reserve(screenSpaceVertices.size());
-
-        for (const auto& screenVertex : screenSpaceVertices) {
-            vertices.push_back(ConvertTo3DSpace(screenVertex, z, screenWidth, screenHeight));
-        }
-
-        return vertices;
-    }
-};
-
-struct S_Shader {
-    unsigned int ID;
-};
-
-void checkCompileErrors(unsigned int shader, std::string type);
-S_Shader new_shader(const char* vertexPath, const char* fragmentPath);
-void use_shader(S_Shader shader);
 
 // this is so useless
 enum FilterParams {   
@@ -87,13 +37,6 @@ struct Colour {
     float a;
 };
 
-typedef struct DrawParams {
-    float x, y;
-    float sx, sy;
-    float r;
-    float ox, oy;
-} DrawParams;
-
 class Graphics {
 public:
 
@@ -103,10 +46,15 @@ public:
         int width, height, nrChannels;
         unsigned char* data;
         glm::mat4 transform;
-        float sx, sy;
-        bool scaled = false;
+        float px, py;
+        bool pos_set = false;
+        // default values for scale
+        float sx = 1.f, sy = 1.f;
+        // rotation can never be negative so set to this initially
+        float r = -1.f;
+        float ox = 0.f, oy = 0.f;
 
-        void set_position(float x, float y, int screenWidth, int screenHeight);
+        void set_position(float x, float y);
         void scale(float sx, float sy);
         void rotate(float r);
     };
@@ -119,7 +67,7 @@ public:
     Texture loadImage(const char* path, TextureFilter filter = {FilterParams::LINEAR, FilterParams::LINEAR});
 
     void drawImage(Texture& texture, float x, float y);
-    void drawImage(Texture texture, DrawParams params);
+    void drawImage(Texture texture, float x, float y, float sx, float sy, float r, float ox, float oy);
 private:
     void bind_and_draw(const Texture& texture);
     void setupBuffers();
